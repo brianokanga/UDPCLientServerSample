cbf81f2fd077b8060ace3cd2f9a925c03901be45
Brian <briannokanga@gmail.com>
2022-04-25 18:26:12 +0300

    Add project files.

---
+++ UDPCLientServerSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPCLientServerSample
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var server = new UDPServer();
+			server.Initialize();
+			server.StartMessageLoop();
+			Console.WriteLine("Hello World!");
+
+			var client = new UDPClient();
+			client.Initialize(IPAddress.Loopback, UDPServer.PORT);
+			client.StartMessageLoop();
+			await client.Send(Encoding.UTF8.GetBytes("Hello"));
+			Console.WriteLine("Message sent!");
+		}
+	}
+}
---
+++ UDPCLientServerSample/UDPServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPCLientServerSample
+{
+    public class UDPServer
+    {
+        public const int PORT = 5000;
+
+        private Socket _socket;
+        private EndPoint _ep;
+
+        private byte[] _buffer_recv;
+        private ArraySegment<byte> _buffer_recv_segment;
+
+        public void Initialize()
+        {
+            _buffer_recv = new byte[4096];
+            _buffer_recv_segment = new(_buffer_recv);
+
+            _ep = new IPEndPoint(IPAddress.Any, PORT);
+
+            _socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.PacketInformation, true);
+            _socket.Bind(_ep);
+        }
+
+        public void StartMessageLoop()
+        {
+            _ = Task.Run(async () =>
+            {
+                SocketReceiveMessageFromResult res;
+                while (true)
+                {
+                    res = await _socket.ReceiveMessageFromAsync(_buffer_recv_segment, SocketFlags.None, _ep);
+                    await SendTo(res.RemoteEndPoint, Encoding.UTF8.GetBytes("Hello back!"));
+                }
+            });
+        }
+
+        public async Task SendTo(EndPoint recipient, byte[] data)
+        {
+            var s = new ArraySegment<byte>(data);
+            await _socket.SendToAsync(s, SocketFlags.None, recipient);
+        }
+    }
+}
